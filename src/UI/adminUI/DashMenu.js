ca03dbef36cc6c3a6b136603677c8920d116d95a
Yuhua PU <yuhua.pu@uqconnect.uq.edu.au>
2019-11-25 02:01:05 +1000

    add course form structure completed

--- src/UI/adminUI/DashMenu.js
+++ src/UI/adminUI/DashMenu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 import "../styles/dashMenu.scss";
 
@@ -10,28 +11,55 @@ function DashMenu(props) {
             <div className="row">
                 <h5 className="col-md">{props.menuText[0]}</h5>
             </div>
+
             <div className="menu-container">
                 <div className="row">
                     <div className="col-md-4" >
+<<<<<<< HEAD
                         <div className="menu-button" >
                             <MenuButton
                                 menuText = {props.menuText[1]}
                             />
                         </div>
+=======
+                        <Link>
+                            <MenuButton
+                                menuText={props.menuText[1]}
+                            />
+                        </Link>
+>>>>>>> add course form structure completed
                     </div>
+
                     <div className="col-md-4" >
+<<<<<<< HEAD
                         <div className="menu-button" >
                             <MenuButton 
                             menuText = {props.menuText[2]} 
                             />
                         </div>
+=======
+                        <Link to="/admin/dashboard/addcourse">
+                            <MenuButton
+                                menuText={props.menuText[2]}
+                            />
+                        </Link>
+>>>>>>> add course form structure completed
                     </div>
+
                     <div className="col-md-4" >
+<<<<<<< HEAD
                         <div className="menu-button" >
                             <MenuButton 
                                 menuText = {props.menuText[3]}
                             />
                         </div>
+=======
+                        <Link>
+                            <MenuButton
+                                menuText={props.menuText[3]}
+                            />
+                        </Link>
+>>>>>>> add course form structure completed
                     </div>
                 </div>
             </div>
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -4,7 +4,11 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from '../UI/Homepage';
 import AdminLogin from '../login/AdminLogin';
 import Admin from "../admin/Admin";
+<<<<<<< HEAD
 import AdminCourseList from "../course/adminCourse/components/AdminCourseList";
+=======
+import AddCourse from "../UI/adminUI/addModal/AddCourse";
+>>>>>>> add course form structure completed
 
 const Routes = () => {
     return (
@@ -12,7 +16,11 @@ const Routes = () => {
             <Switch>
                 <Route exact path="/admin" component={AdminLogin} />
                 <Route exact path="/admin/dashboard" component={Admin} />
+<<<<<<< HEAD
                 <Route path="/admin/dashboard/courselist" component={AdminCourseList} />
+=======
+                <Route exact path="/admin/dashboard/addcourse" component={AddCourse} />
+>>>>>>> add course form structure completed
                 <Route path="/" component={Homepage} />
             </Switch>
         </Router>
