4db6f60c2a543e1aa3d4ed650b284f82af251275
SlackEric <ericye1001@gmail.com>
2019-12-10 23:58:39 +1000

    Remove semantic-ui and add my own login loader css
    Remove semantic-ui and add my own login loader css.

--- src/login/AdminLogin.js
+++ src/login/AdminLogin.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./styles/adminLogin.scss";
-import { Segment } from "semantic-ui-react";
 
 import loginIcon from "../resource/img/login-icon.png";
 import { login } from "../utils/api/auth";
 import { setToken } from "../utils/authentication";
-import { ADMIN_DASHBOARD_URL } from '../routes/URLMap';
+import { ADMIN_DASHBOARD_URL } from "../routes/URLMap";
 
 const AdminLogin = (props) => {
   const [loginStates, setLoginStates] = useState({
     email: "",
     password: "",
     isLoading: false,
+    isErrShowing: false,
     ...props
   });
 
@@ -25,56 +25,76 @@ const AdminLogin = (props) => {
     setLoginStates({ ...loginStates, isLoading: true });
     event.preventDefault();
   };
-  
+
   useEffect(() => {
     if (loginStates.isLoading) {
-      login(loginStates.email, loginStates.password).then(token => {
-        setToken(token);
-        setLoginStates({ ...loginStates, isLoading: false });
-        const locationState = loginStates.location.state;
-        const redirectTo = (locationState && locationState.from) || ADMIN_DASHBOARD_URL;
-        loginStates.history.replace(redirectTo);
-      }).catch(()=>setLoginStates({...loginStates, isLoading: false}));
+      login(loginStates.email, loginStates.password)
+        .then(token => {
+          setToken(token);
+          setLoginStates({
+            ...loginStates,
+            isLoading: false,
+            isErrShowing: false
+          });
+          const locationState = loginStates.location.state;
+          const redirectTo =
+            (locationState && locationState.from) || ADMIN_DASHBOARD_URL;
+          loginStates.history.replace(redirectTo);
+        })
+        .catch(() =>
+          setLoginStates({
+            ...loginStates,
+            isLoading: false,
+            isErrShowing: true
+          })
+        );
     }
   }, [loginStates]);
 
+  const overlay = loginStates.isLoading ? "overlay" : "";
+  const spinnerClass = loginStates.isLoading
+    ? "spinner-border spinner-location"
+    : "";
+  const style = loginStates.isErrShowing ? {} : { display: "none" };
+  const inputError = loginStates.isErrShowing ? "has-error" : "";
 
   return (
-     <div className="row">
-     <div className="col-sm-2" />
-     <form className="form col-sm-8">
-       <img src={loginIcon} className="form-icon" alt="loginIcon" />
-       <h4 className>Admin Login</h4>
-       <Segment basic loading={loginStates.isLoading}>
-         <div className="form-group">        
-           <input
-             onChange={handleChange}
-             name="email"
-             value={loginStates.email}
-             type="email"
-             className="form-control"
-             id="email"
-             placeholder="Username"
-           />
-         </div>
-         <div className="form-group">           
-           <input
-             onChange={handleChange}
-             name="password"
-             value={loginStates.password}
-             type="password"
-             className="form-control"
-             id="pwd"
-             placeholder="Password"
-           />
-         </div>
-         <button onClick={loginUser}>
-           Login
-         </button>
-       </Segment>
-     </form>
-     <div className="col-sm-2" />
-   </div>     
+    <div className="row">
+      <div className="col-sm-2" />
+      <form className={`form col-sm-8 ${overlay}`}>
+        <img src={loginIcon} className="form-icon" alt="loginIcon" />
+        <h4 className>Admin Login</h4>
+
+        <div className="form-group">
+          <input
+            onChange={handleChange}
+            name="email"
+            value={loginStates.email}
+            type="email"
+            className={`form-control ${inputError}`}
+            id="email"
+            placeholder="Username"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            onChange={handleChange}
+            name="password"
+            value={loginStates.password}
+            type="password"
+            className={`form-control ${inputError}`}
+            id="pwd"
+            placeholder="Password"
+          />
+        </div>
+        <div style={style}>
+          <label className="error-text">Invalid username or password!</label>
+        </div>
+        <button onClick={loginUser}>Login</button>
+        <div className={spinnerClass}></div>
+      </form>
+      <div className="col-sm-2" />
+    </div>
   );
 };
 
