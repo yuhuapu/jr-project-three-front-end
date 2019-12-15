48294dac77bfb99d7c36372460b5d5174c0785a0
Yuhua PU <yuhua.pu@uqconnect.uq.edu.au>
2019-12-16 01:03:11 +1000

    merged login to the current branch

--- src/utils/api/axios.js
+++ src/utils/api/axios.js
@@ -1,27 +1,11 @@
 import axios from 'axios';
 import { getToken } from '../authentication';
 
-<<<<<<< HEAD
-axios.defaults.baseURL = 'http://localhost:4000/api/v1';
-=======
 axios.defaults.baseURL = 'https://jr-project-three-back-end.herokuapp.com/api/v1';
->>>>>>> master
 
 const appendAuthToken = config => {
     const jwtToken = getToken();
     const Authorization = jwtToken && `Bearer ${jwtToken}`;
-<<<<<<< HEAD
-    return { ...config, headers: { Authorization, ...config.headers } };
-}
-
-export const get = (url, config = {}) => axios.get(url, appendAuthToken(config));
-
-export const post = (url, data, config = {}) => axios.post(url, data, appendAuthToken(config));
-
-export const put = (url, data, config = {}) => axios.put(url, data, appendAuthToken(config));
-
-export const del = (url, config = {}) => axios.delete(url, appendAuthToken(config));
-=======
     return {...config, headers: { Authorization, ...config.header}};
 };
 
@@ -36,4 +20,3 @@ export const put = (url, data, config = {}) =>
 
 export const del = (url, config = {}) =>
     axios.delete(url, appendAuthToken(config));
->>>>>>> master
--- src/utils/authentication.js
+++ src/utils/authentication.js
@@ -1,16 +1,4 @@
 import jwt from 'jsonwebtoken';
-<<<<<<< HEAD
-
-const JWT_TOKEN_NAME = "jwt_token_jr_lms";
-
-export const getToken = () => localStorage.getItem(JWT_TOKEN_NAME);
-
-export const setToken = token => localStorage.setItem(JWT_TOKEN_NAME, token);
-
-export const removeToken = () => localStorage.removeItem(JWT_TOKEN_NAME);
-
-export const isLoggedIn = () => {
-=======
 const JWT_KEY = 'jwt';
 
 export const setToken = token => {
@@ -22,7 +10,6 @@ export const getToken = () => {
 }
 
 export const isAuthenticated = () => {
->>>>>>> master
     const token = getToken();
 
     if (!token) return false;
